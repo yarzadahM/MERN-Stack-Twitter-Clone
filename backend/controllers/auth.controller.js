94c89a5f51b7a403c9437039a8247dbdc264316d
YarzadahM <marziayarzadah32@gmail.com>
2024-08-31 19:55:26 +0430

    add controller file for auth

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup=async (req,res)=>{
+    res.json({
+        data:"signup endpoint"
+    })
+}
+export const login=async (req,res)=>{
+    res.json({
+        data:"login endpoint"
+    })
+}
+export const logout=async (req,res)=>{
+    res.json({
+        data:"logout endpoint"
+    })
+}
